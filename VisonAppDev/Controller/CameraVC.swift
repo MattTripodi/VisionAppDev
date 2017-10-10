3202c7df6f614591b75ddd39d869c6f0b18f4e34
Matt Tripodi <mtripodi1027@gmail.com>
2017-10-10 14:14:46 -0400

    Setup project folders

--- VisonAppDev/Controller/CameraVC.swift
+++ VisonAppDev/Controller/CameraVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CameraVC.swift
 //  VisonAppDev
 //
 //  Created by Matt Tripodi on 10/10/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CameraVC: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
